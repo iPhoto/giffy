f857e95525ef671a9b1e592ace33a7f81d446324
Doug Rubino <doug.rubino@gmail.com>
2013-04-12 23:47:40 -0400

    Initial commit of the giffy web api

---
+++ web/Giffy/Giffy/Controllers/Api/Account/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+using Giffy.Filters;
+using Giffy.Models;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+
+namespace Giffy.Controllers.Api
+{
+    [InitializeApiMembershipAttribute]
+    public class LoginController : ApiController
+    {
+        [AllowAnonymous]
+        public bool Post(LoginModel model)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password))
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, false);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ web/Giffy/Giffy/Models/ModelContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Giffy.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("DefaultConnection") { }
+    }
+
+    public class ModelDatabaseInitializer : DropCreateDatabaseAlways<ModelContext>
+    {
+        protected override void Seed(ModelContext context)
+        {
+            
+        }
+    }
+}
