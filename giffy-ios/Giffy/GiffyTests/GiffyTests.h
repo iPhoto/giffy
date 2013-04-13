02825d91f01e091dbf4fc459f4f9a3076e59d543
Kyle Olivo <kyleolivo@gmail.com>
2013-04-12 22:53:16 -0400

    Created initial iOS app.

---
+++ giffy-ios/Giffy/GiffyTests/GiffyTests.h
@@ -0,0 +1,13 @@
+//
+//  GiffyTests.h
+//  GiffyTests
+//
+//  Created by Kyle Olivo on 4/12/13.
+//  Copyright (c) 2013 The Giffy Group. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GiffyTests : SenTestCase
+
+@end
